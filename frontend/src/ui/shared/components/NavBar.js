eb7e1c1aaaf47f86ba476695638542f65c312275
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-01 16:49:09 -0600

    added static Navbar

--- frontend/src/ui/shared/components/NavBar.js
+++ frontend/src/ui/shared/components/NavBar.js
@@ -2,11 +2,9 @@ import React from "react";
 import {Figure, Nav} from "react-bootstrap";
 import FigureImage from "react-bootstrap/FigureImage";
 import FoodieLogo from '../imgs/allergicfoodieslogo.png'
-import Navbar from "react-bootstrap/Navbar";
-import {useSelector} from "react-redux";
+
 
 export function NavBar () {
-    const auth = useSelector(state => state.auth);
     return (
         <>
             <div className='text-center'>
@@ -18,35 +16,25 @@ export function NavBar () {
                         height={100}
                     />
                 </Figure>
-                <NavBar>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="justify-content-center" activeKey="/home">
+                <Nav className='justify-content-center' activeKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="/home" className={'text-dark'}>My Homepage</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2" className={'text-dark'}>My Account</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2" className={'text-dark'}>My Favorite</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2" className={'text-dark'}>Login</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2" className={'text-dark'}>Logout</Nav.Link>
+                    </Nav.Item>
+                </Nav>
 
-                            <Nav.Item>
-                                <Nav.Link href="/home" className={'text-dark'}>My Homepage</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                {auth === null && (
-                                 <>
-                                    <Nav.Link eventKey="link-1" className={'text-dark'}>Login</Nav.Link>
-                                 </>
-                                )}
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="link-2" className={'text-dark'}>My Account</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="link-2" className={'text-dark'}>My Favorite</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="link-2" className={'text-dark'}>Logout</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </NavBar>
             </div>
-
         </>
     )
 }
