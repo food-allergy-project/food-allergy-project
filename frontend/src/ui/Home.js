a0059792239e61d392f6ac8edfbd74eba1800a73
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-01 15:54:59 -0600

    added NavBar.js and logo

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,8 +1,10 @@
 import React from "react"
+import {NavBar} from "./shared/components/NavBar";
 
 export const Home = () => {
     return (
         <>
+            <NavBar />
             <h1>Home</h1>
         </>
     )
