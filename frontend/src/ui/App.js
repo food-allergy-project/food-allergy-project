54622c62b12f610672fb635fbd160c7e480126a3
Thanthip Fawn Orbesen <torbesen@cnm.edu>
2022-06-02 13:52:31 -0600

    adding Personalized Homepage

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {PersonlizedHomePage} from "./PersonlizedHomePage";
 
 export const App = () => (
     <>
@@ -10,6 +11,7 @@ export const App = () => (
             <Routes>
                 <Route  path='/' element={<Home />} />
                 <Route path='*' element={<FourOhFour />} />
+                <Route path='/personalized-homepage' element={<PersonlizedHomePage/>} />
             </Routes>
         </BrowserRouter>
 
---
+++ frontend/src/ui/PersonlizedHomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const PersonlizedHomePage = () => {
+    return(
+        <>
+        <div>
+            <h1>This is your Personalized Homepage</h1>
+
+
+        </div>
+        </>
+    )
+}
