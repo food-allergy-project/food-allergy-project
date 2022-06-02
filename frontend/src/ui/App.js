84565959798cf90c54cb4117e05eb530aa353d22
Ron Schneider <rschneider9@cnm.edu>
2022-06-02 14:44:00 -0600

    recipe page static in react save

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,13 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
-
+import {RecipePage} from './RecipePage'
+import {NavBar} from '../ui/shared/components/NavBar'
+import "./App.css"
+ 
 export const App = () => (
     <>
         <BrowserRouter>
+          <NavBar/>
             <Routes>
-                <Route  path='/' element={<Home />} />
-                <Route path='*' element={<FourOhFour />} />
+                <Route path='/' element={<Home />} />
+                <Route path='*' element={<FourOhFour/>} />
+                <Route path='/recipes'element={<RecipePage />} />
+
             </Routes>
         </BrowserRouter>
 
--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,10 +1,8 @@
 import React from "react"
-import {NavBar} from "./shared/components/NavBar";
 
 export const Home = () => {
     return (
         <>
-            <NavBar />
             <h1>Home</h1>
         </>
     )
