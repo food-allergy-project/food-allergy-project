b7f1545e9fd7552d8c4d98db57e63f6a8eaa04a1
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-02 14:10:41 -0600

    Changed button and image's size.

--- frontend/src/ui/LandingPage/TakeQuizButtonAndImg.js
+++ frontend/src/ui/LandingPage/TakeQuizButtonAndImg.js
@@ -1,20 +1,21 @@
 import React from "react";
 import {Button, Container, Figure} from "react-bootstrap";
 import FigureImage from "react-bootstrap/FigureImage";
+import '../App.css';
 
 export function TakeQuizButtonAndImg () {
     return (
         <Container className='p-5 my-5'>
             <div className='text-center'>
-                <Button variant="success" size="lg">Take Quiz</Button>
+                <Button variant="success" className='buttonBlockSize'>Take Quiz</Button>
             </div>
-            <div className='p-5 text-center'>
+            <div className='p-5 my-5 text-center'>
                 <Figure>
                     <FigureImage
                         src="https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_960_720.jpg"
                         alt="Food1"
-                        width={700}
-                        height={700}
+                        width={800}
+                        height={800}
                     />
                 </Figure>
             </div>
