4c180f911756c9668338b81caed2ee5635e25579
Ron Schneider <rschneider9@cnm.edu>
2022-06-03 11:42:09 -0600

    adding for restart

--- frontend/src/ui/FavoritePage.js
+++ frontend/src/ui/FavoritePage.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
+import { } from 'react-bootstrap'
 
 export function FavoritePage () {
   return (
     <>
-      <Container className="text-center">
-        <h1>
-          Your Favorites
-        </h1>
-      </Container>
+
     </>
   )
 }
