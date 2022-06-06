3759ee1ba6acfe3842a7e55dc2d970c3f45fe819
Ron Schneider <rschneider9@cnm.edu>
2022-06-06 11:24:18 -0600

    finished static footer

--- frontend/src/ui/shared/components/Footer.js
+++ frontend/src/ui/shared/components/Footer.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {Figure} from "react-bootstrap";
+import FigureImage from "react-bootstrap/FigureImage";
+import GitHubIcon from '../imgs/GitHubIcon.png'
+import LinkInLogo from '../imgs/LInkedInIcon.png'
+import "../../Footer.css"
+import FoodieLogo from "../imgs/JustCarrotIcon.png";
 
 
 
@@ -6,18 +12,33 @@ import React from "react";
 export function Footer() {
     return (
         <>
-            <div className="container-fluid text-center text-md-left bg-secondary p-3">
+            <div className="container-fluid text-center text-md-left bg p-3 ">
                 <div className="row">
-                    <div className="col-md-6 mt-md-0 mt-3 p-3">
-                        <h5 className="text-uppercase">Allergic Foodie</h5>
-                        <p>Here you can use rows and columns to organize your footer content.</p>
+                    <div className="col-md-5 mt-md-0 mt-3 p-3 creators">
+                        <Figure>
+                            <FigureImage
+                                src= {FoodieLogo}
+                                alt="Allergic Foodies Logo"
+                                width={500}
+                                height={100}
+                            />
+                        </Figure>
+                        <h2>Allergic Foodies Creators </h2>
+
+                        <p>June 2022 </p>
                     </div>
-
-                    <hr className="clearfix w-100 d-md-none pb-0"/>
-
-                    <div className="col-md-3 mb-md-0 mb-3">
-                        <h5 className="text-uppercase">Creators</h5>
-                        <ul className="list-unstyled">
+                    <div className= "col-md-3 mb-md-0 mb-3 p-4">
+                        <ul className="list-unstyled footer">
+                            <li>
+                            <Figure>
+                                <FigureImage
+                                    className='LinkedInIcon'
+                                    width='100%'
+                                    src={LinkInLogo}
+                                    alt=" LinkedIn"
+                                />
+                            </Figure>
+                            </li>
                             <li><a href="https://www.linkedin.com/in/sierra-moya/">Sierra Moya</a></li>
                             <li><a href="https://www.linkedin.com/in/thanthip-siriwet-orbesen/">Fawn Orbesen</a></li>
                             <li><a href="https://www.linkedin.com/in/patiphan-orbesen-923631235/">Pat Orbesen</a></li>
@@ -25,13 +46,20 @@ export function Footer() {
                         </ul>
                     </div>
 
-                    <div className="col-md-3 mb-md-0 mb-3">
-                        <h5 className="text-uppercase">Github</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#!">Link 1</a></li>
-                            <li><a href="#!">Link 2</a></li>
-                            <li><a href="#!">Link 3</a></li>
-                            <li><a href="#!">Link 4</a></li>
+                    <div className="col-md-3 mb-md-0 mb-3 p-4">
+                        <Figure>
+                            <FigureImage
+                                className='GitHubIcon'
+                                width='100%'
+                                src={GitHubIcon}
+                                alt="GithubIcon"
+                            />
+                        </Figure>
+                        <ul className="list-unstyled footer">
+                            <li><a href="https://github.com/sierramoya">Sierra Moya</a></li>
+                            <li><a href="https://github.com/thanthip-work">Fawn Orbesen</a></li>
+                            <li><a href="https://github.com/Ps-Orbesen">Pat Orbesen</a></li>
+                            <li><a href="https://github.com/rschneider9">Ron Schneider</a></li>
                         </ul>
                     </div>
                 </div>
