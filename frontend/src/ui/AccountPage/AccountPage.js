73ebff60242c2a6bfbb8d1c7118ab04920c17483
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-06 14:54:24 -0600

    Added MyRecipes.js

--- frontend/src/ui/AccountPage/AccountPage.js
+++ frontend/src/ui/AccountPage/AccountPage.js
@@ -1,10 +1,12 @@
 import React from "react"
 import {UserProfileSection} from "./UserProfileSection";
+import {MyRecipes} from "./MyRecipes";
 
 export const AccountPage = () => {
     return (
         <>
             <UserProfileSection />
+            <MyRecipes />
         </>
     )
 }
