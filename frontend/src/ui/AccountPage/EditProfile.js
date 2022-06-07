eefbe478d066eba86d1c1925a313bbff8ee5e43e
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-07 15:21:41 -0600

    edited EditProfile.js and added EditProfileForm.js

--- frontend/src/ui/AccountPage/EditProfile.js
+++ frontend/src/ui/AccountPage/EditProfile.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Modal} from "react-bootstrap";
+import {EditProfileForm} from "./EditProfileForm";
+
+
+
 
 export function EditProfile () {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <div>
-            <button type='button' className='btn btn-outline-dark btn-sm'> Edit profile</button>
-        </div>
-    )
+        <>
+            <Button variant="success" onClick={handleShow}>
+                Edit Profile
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <EditProfileForm />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
+
 
