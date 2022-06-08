1e621fa2f976a8da02f16b56c99bae5020832532
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-07 22:23:39 -0600

    edited SignIn.js for Redux

--- frontend/src/ui/SignIn.js
+++ frontend/src/ui/SignIn.js
@@ -1,8 +1,74 @@
 import React from "react";
 import {Button, Container} from "react-bootstrap";
 import './App.css'
+import {useDispatch} from "react-redux";
+import * as Yup from "yup";
+import {httpConfig} from "../utils/httpConfig";
+import jwtDecode from "jwt-decode";
+import {getAuth} from "../store/auth";
+import {Formik} from "formik";
 
-export function SignIn () {
+export const SignInForm = () => {
+    const dispatch = useDispatch()
+
+    const validator = Yup.object().shape({
+        profileEmail: Yup.string()
+            .email("please provide a valid email")
+            .required('email is required'),
+        profilePassword: Yup.string()
+            .required("password is required")
+            .min(8, "password must be at least eight characters")
+    });
+
+
+    //the initial values object defines what the request payload is.
+    const signIn = {
+        profileEmail: "",
+        profilePassword: ""
+    };
+
+    const submitSignIn = (values, {resetForm, setStatus}) => {
+        httpConfig.post("/apis/sign-in/", values)
+            .then(reply => {
+                let {message, type} = reply;
+                setStatus({message, type});
+                if(reply.status === 200 && reply.headers["authorization"]) {
+                    window.localStorage.removeItem("authorization");
+                    window.localStorage.setItem("authorization", reply.headers["authorization"]);
+                    resetForm();
+                    let jwtToken = jwtDecode(reply.headers["authorization"])
+                    dispatch(getAuth(jwtToken))
+                }
+                setStatus({message, type});
+            });
+    };
+
+    return (
+        <>
+            <Formik
+                initialValues={signIn}
+                onSubmit={submitSignIn}
+                validationSchema={validator}
+            >
+                {SignInFormContent}
+            </Formik>
+        </>
+    )
+};
+
+function SignInFormContent (props) {
+    const {
+        status,
+        values,
+        errors,
+        touched,
+        dirty,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset
+    } = props;
     return (
         <>
             <Container className='text-center border border-dark border-2 rounded p-3 col-md-6 pt-3 my-5 bg-success bg-opacity-10 bg-gradient'>
@@ -16,19 +82,47 @@ export function SignIn () {
 
                 <div>
 
-                    <form className='mx-auto col-form-label-lg centerBlockParent'>
+                    <form onSubmit={handleSubmit} className='mx-auto col-form-label-lg centerBlockParent'>
 
                         <div className='form-floating mb-1 col-md-8'>
-                                <input type='email' className='form-control border border-dark border-2' placeholder='email' name='email'/>
-                                <label className='form-label'>Email: </label>
-                            </div>
+                                <input
+                                    value={values.profileEmail}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    type='email'
+                                    className='form-control border border-dark border-2'
+                                    placeholder='email'
+                                    name='profileEmail'/>
+                                <label htmlFor='profileEmail' className='form-label'>Email: </label>
+                        </div>
+                        {
+                            errors.profileEmail && touched.profileEmail && (
+                                <div className="alert alert-danger">
+                                    {errors.profileEmail}
+                                </div>
+                            )
+                        }
 
                         <div className='form-floating mb-1 col-md-8'>
-                                <input type='text' className='form-control border border-dark border-2' placeholder='password' name='password'/>
-                                <label className='form-label'>Password: </label>
-                            </div>
+                                <input
+                                    type='password'
+                                    value={values.profilePassword}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className='form-control border border-dark border-2'
+                                    placeholder='password'
+                                    name='profilePassword'/>
+                                <label htmlFor='profilePassword' className='form-label'>Password: </label>
+                        </div>
+                        {
+                            errors.profilePassword && touched.profilePassword && (
+                                <div className="alert alert-danger">
+                                    {errors.profilePassword}
+                                </div>
+                            )
+                        }
 
-                        </form>
+                    </form>
                 </div>
 
                 <div className='text-center py-5'>
