5d19f7d665cb7fe3506d2e88abd29950f94521a5
Ron Schneider <rschneider9@cnm.edu>
2022-06-09 14:26:10 -0600

    adding a connected redux to favorite recipes

--- frontend/src/ui/YourFavorited/FavoriteCards.js
+++ frontend/src/ui/YourFavorited/FavoriteCards.js
@@ -3,29 +3,32 @@ import {Card, Col} from "react-bootstrap";
 import "./FavoritedCardRecipe.css"
 
 
-export function FavoriteRecipeCardLunch({recipes}) {
-
-
+export function FavoriteCards(props) {
+    console.log(props)
+    const {recipes} = props
+//todo if recipes is empty display some message// say something like no recipes found, make sure to add recipes//
     return (
+
         <>
             {
-                /*recipes.map((recipe) => {
+                recipes.map((recipe) => {
                     return (
                         <Col className="justify-content-center align-content-center">
                             <Card bg='light' text='dark' className="favoriteRecipeCardSize mt-5">
                                 <Card.Img variant="top" className="favoriteRecipeCardImg"
                                           src={
-                                              recipe.img
+                                              recipe.recipeImage
                                           }/>
                                 <Card.Body>
-                                    <Card.Title className="FavoriteRecipeCards">{
-                                        recipe.name
-                                    }</Card.Title>
+                                    <Card.Title className="FavoriteRecipeCards">
+                                        {
+                                            recipe.recipeTitle
+                                        }</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
                     )
-                })*/
+                })
             }
         </>
     )
--- frontend/src/ui/YourFavorited/FavoritePage.js
+++ frontend/src/ui/YourFavorited/FavoritePage.js
@@ -1,119 +1,124 @@
 import React, {useEffect, useState} from 'react'
 import {Container, Row, Tab, Tabs} from 'react-bootstrap'
-import {FavoriteRecipeCardsBreakfast} from "./YourFavoriteCardsBreakfast";
-import {FavoriteRecipeCardsDinner} from "./YourFavoriteCardsDinner";
-import {FavoriteRecipeCardLunch} from "./YourFavoriteCard";
-import {FavoriteRecipeCardLunchDessert} from "./YourFavoriteCardsDessert";
-import {FavoriteRecipeCardsSnack} from "./YourFavoriteCardsSnack";
+import {FavoriteCards} from "./FavoriteCards";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchAuth} from "../../store/auth";
-import favoritedRecipes, {fetchFavoritedRecipesByProfileId} from "../../store/favoritedRecipes";
+import {fetchFavoritedRecipesByProfileId} from "../../store/favoritedRecipes";
+import auth, {fetchAuth} from "../../store/auth";
+import {fetchAllRecipes} from "../../store/recipes";
 
 
 function Sonnet() {
     return null;
 }
 
-export function FavoritePage ({recipes}) {
+
+export function FavoritePage() {
     const [key, setKey] = useState('');
 
     const auth = useSelector(state => state.auth);
+
     const recipeCategories = useSelector(({favoritedRecipes}) => {
+        const initialValue = {
+            breakfastRecipes: [],
+            lunchRecipes: [],
+            dinnerRecipes: [],
+            dessertRecipes: [],
+            snackRecipes: []
+        }
         if (favoritedRecipes === null) {
-         return  []
+            return initialValue
+
         }
 
-        //use reduce on favoritedrecipes make sure to set initial value to an object with the fields breakfast
-        // lunch, dinner, dessert, and snack. making sure that each field is an empty array.
-        //Using recipe category determen what field to push the current value into
-        //return result from reduce
+        for (let recipe of favoritedRecipes) {
 
+            if (recipe.recipeCategory === "breakfast") {
+                initialValue.breakfastRecipes.push(recipe)
+            } else if
+            (recipe.recipeCategory === "lunch") {
+                initialValue.lunchRecipes.push(recipe)
+            } else if
+            (recipe.recipeCategory === "dinner") {
+                initialValue.dinnerRecipes.push(recipe)
+            } else if
+            (recipe.recipeCategory === "dessert") {
+                initialValue.dessertRecipes.push(recipe)
+            } else if
+            (recipe.recipeCategory === "snack") {
+                initialValue.snackRecipes.push(recipe)
+            }
+        }
 
+        return initialValue
     })
-    console.log(auth)
-    const dispatch = useDispatch()
-    const effects = () => {
-        dispatch(fetchAuth());
-        dispatch(fetchFavoritedRecipesByProfileId())
-
-    };
-    const inputs = [];
-    useEffect(effects, inputs);
 
 
-/*
+console.log(recipeCategories.dessertRecipes)
+console.log(auth)
+const dispatch = useDispatch()
+const effects = () => {
+    dispatch(fetchAuth());
+    dispatch(fetchFavoritedRecipesByProfileId())
+    dispatch(fetchAllRecipes())
+};
+const inputs = [];
+useEffect(effects, inputs);
+    React.useEffect(effects, [dispatch])
 
 
+return (
+    <>
+        <Container className='m-5'>
+            <h1 className='text-md-start'>Your Favorites</h1>
+        </Container>
+        <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+            className="m-5 text-center"
+        >
+            <Tab eventKey="Breakfast" title="Breakfast">
+                <Container>
+                    <Row>
+                         <FavoriteCards recipes={recipeCategories.breakfastRecipes}/>
+                        <Sonnet/>
+                    </Row>
+                </Container>
+            </Tab>
+            <Tab eventKey="Lunch" title="Lunch">
+                <Container>
+                    <Row>
+                        <FavoriteCards recipes={recipeCategories.lunchRecipes}/>
+                        <Sonnet/>
+                    </Row>
+                </Container>
 
-    for (const item of items) {
-        if (item.name === 'breakfast') push acc.{}.push{
-            return item
-        }
-    }
-
-*/
-
-
-
-
-    //Loop over favorited-recipes from redux's store
-
-
-    //In loop, check recipe.category and if recipe category matches recipe from store add to that list.
-    //breakfastrecipes.push(recipe)
-    return (
-        <>
-            <Container className='m-5'>
-                <h1 className='text-md-start'>Your Favorites</h1>
-            </Container>
-            <Tabs
-                id="controlled-tab-example"
-                activeKey={key}
-                onSelect={(k) => setKey(k)}
-                className="m-5 text-center"
-            >
-                <Tab eventKey="Breakfast" title="Breakfast">
-                    <Container>
-                        <Row>
-                           {/* <FavoriteRecipeCard recipes={recipe}/>*/}
-                            <Sonnet/>
-                        </Row>
-                    </Container>
-                </Tab>
-                <Tab eventKey="Lunch" title="Lunch">
-                    <Container>
-                        <Row>
-                            <FavoriteRecipeCardLunch/>
-                            <Sonnet/>
-                        </Row>
-                    </Container>
-
-                </Tab>
-                <Tab eventKey="Dinner" title="Dinner">
-                    <Container>
-                        <Row>
-                            <FavoriteRecipeCardsDinner/>
-                            <Sonnet/>
-                        </Row>
-                    </Container>
-                </Tab>
-                <Tab eventKey="Dessert" title="Dessert">
-                    <Container>
-                        <Row>
-                            <FavoriteRecipeCardLunchDessert/>
-                            <Sonnet/>
-                        </Row>
-                    </Container>
-                </Tab>
-                <Tab eventKey="Snacks" title="Snacks">
-                    <Container>
-                        <Row>
-                            <FavoriteRecipeCardsSnack/>
-                            <Sonnet/>
-                        </Row>
-                    </Container>
-                </Tab>
-            </Tabs>
-        </>
-    )
+            </Tab>
+            <Tab eventKey="Dinner" title="Dinner">
+                <Container>
+                    <Row>
+                        <FavoriteCards recipes={recipeCategories.dinnerRecipes}/>
+                        <Sonnet/>
+                    </Row>
+                </Container>
+            </Tab>
+            <Tab eventKey="Dessert" title="Dessert">
+                <Container>
+                    <Row>
+                        <FavoriteCards recipes={recipeCategories.dessertRecipes}/>
+                        <Sonnet/>
+                    </Row>
+                </Container>
+            </Tab>
+            <Tab eventKey="Snacks" title="Snacks">
+                <Container>
+                    <Row>
+                        <FavoriteCards recipes={recipeCategories.snackRecipes}/>
+                        <Sonnet/>
+                    </Row>
+                </Container>
+            </Tab>
+        </Tabs>
+    </>
+)
 }
