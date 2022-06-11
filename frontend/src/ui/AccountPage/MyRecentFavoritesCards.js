99952b886121e521f3eb66ccb7ae70a96eddec27
Patiphan Orbesen <porbesen@cnm.edu>
2022-06-11 11:12:22 -0600

    Successfully display favorites recipes on account page

--- frontend/src/ui/AccountPage/MyRecentFavoritesCards.js
+++ frontend/src/ui/AccountPage/MyRecentFavoritesCards.js
@@ -2,31 +2,29 @@ import React from "react";
 import '../AccountPage/AccountPage.css'
 import {Card, Col} from "react-bootstrap";
 
-export function MyRecentFavoritesCards (props) {
+export function MyRecentFavoritesCards(props) {
     const {myRecentFavorites} = props
 
     return (
         <>
-            {
-                myRecentFavorites.map((myRecentFavorites) => {
-                    return (
-                        <Col>
-                            <Card bg='light' text='dark' className='myRecipeCardSize mt-4'>
-                                <Card.Img variant="top" className='myRecipeCardImg'
-                                          src={
-                                            myRecentFavorites.recipeImage
-                                          }
-                                />
-                                <Card.Body>
-                                    <Card.Title className='myRecipeCardTitle'>
-                                        {myRecentFavorites.recipeTitle}
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    )
-                })
-            }
+
+
+            <Col>
+                <Card bg='light' text='dark' className='myRecipeCardSize mt-4'>
+                    <Card.Img variant="top" className='myRecipeCardImg'
+                              src={
+                                  myRecentFavorites.recipeImage
+                              }
+                    />
+                    <Card.Body>
+                        <Card.Title className='myRecipeCardTitle'>
+                            {myRecentFavorites.recipeTitle}
+                        </Card.Title>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+
         </>
     )
 }
--- frontend/src/ui/AccountPage/MyRecipes.js
+++ frontend/src/ui/AccountPage/MyRecipes.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Card, Col, Container, Row} from "react-bootstrap";
 import {MyRecipesCards} from "./MyRecipesCards";
 import {MyRecentFavoritesCards} from "./MyRecentFavoritesCards";
-import favoritedRecipes from "../../store/favoritedRecipes";
 
 
 export const MyRecipes = (props) => {
